19847b3c80d3c54e285b92ebc6725defb168887c
Tomasz Bułat <buatss@gmail.com>
2023-05-26 13:25:05 +0200

    simple java import

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,8 @@
-data class Person(
-    val name: String,
-    val age: Int? = null
-)
-
-fun max(a: Int, b: Int): Int {
-    return if(a > b) a else b
-}
+import geometry.shapes.*
 
 fun main(args: Array<String>) {
-    val persons = listOf(
-        Person("Alicja"),
-        Person("Bartek", age = 29)
-    )
-    val oldest = persons.maxBy { it.age ?: 0 }
-    println("Najstarsza osoba: $oldest")
-
-    val max = max(3, 5)
-    println("Max 3 i 5: $max")
+    val rectangle : Rectangle = createRandomRectangle()
+    println("Height: " + rectangle.height)
+    println("Width: " + rectangle.width)
+    println("Is this rectangle a square: " + rectangle.isSquare)
 }
